046803c59592a551497890d55fb08ecba0335470
Georgiy <argeo05@mail.ru>
2023-04-23 15:00:35 +0300

    start of styling nav

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import navbar from './navbar.module.scss'
 
 interface Props {}
 
 function Navbar(props: Props) {
    const {} = props
-
-   return (
-      <p>NavBar</p>
+   const isAuth = true
+   const userName = 'UserName'
+   return ( 
+      <div className={`${navbar.cotainer}`}>
+         <div className={`${navbar.top}`}>
+            <div className={`${navbar.logo}`}>
+               <img className={`${navbar.logoImage}`} src='img/logo.png' width='100px' height='40px'/>
+            </div>
+            <div className={`${navbar.user}`}>
+               <img className={`${navbar.userImage}`} src='img/avatar.png' width='50px' height='50px'/>
+               <span className={`${navbar.userImage}`}>{userName}</span>
+            </div>
+         </div>
+         <nav className={`${navbar.nav}`}>
+            <ul className={`${navbar.navList}`}>
+               <li className={`${navbar.navLi} ${navbar.liProfile}`} >
+                  <Link className={`${navbar.navLink}`} to='/'>Profile</Link>
+               </li>
+               <li className={`${navbar.navLi} ${navbar.liExplore}`} >
+                  <Link className={`${navbar.navLink}`} to='/'>Explore</Link>
+               </li>
+               <li className={`${navbar.navLi} ${navbar.liNotifications}`} >
+                  <Link className={`${navbar.navLink}`} to='/'>Notifications</Link>
+               </li>
+               <li className={`${navbar.navLi} ${navbar.liCreator}`} >
+                  <Link className={`${navbar.navLink}`} to='/'>About creator</Link>
+               </li>
+            </ul>
+         </nav>
+      </div>
    )
 }
 
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,23 +1,23 @@
 import Feed from "../../components/feed/feed"
 import Navbar from "../../components/navbar/navbar"
 import SideBlock from "../../components/side-block/side-block"
-import style from '../../global.module.scss'
+import index from '../../index.module.scss'
 
-interface Props {}
+interface Props { }
 
 function MainPage(props: Props) {
-   const {} = props
+   const { } = props
 
    return (
-      <div className={`page ${style.container}`}>
-         <header className="header">
-            <Navbar />
-         </header>
-         <main>
-            <Feed />
-            <SideBlock />
-         </main>
-      </div>
+      <div className={`${index.page} ${index.container}`}>
+            <header className="header">
+               <Navbar />
+            </header>
+            <main>
+               <Feed />
+               <SideBlock />
+            </main>
+      </div >
    )
 }
 
