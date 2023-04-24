b5dbb6bda764c48baf6e5d75153c8f3ab948452f
Georgiy <argeo05@mail.ru>
2023-04-24 08:55:12 +0300

    nav is done

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,36 +2,36 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import navbar from './navbar.module.scss'
 
-interface Props {}
+interface Props { }
 
 function Navbar(props: Props) {
-   const {} = props
+   const { } = props
    const isAuth = true
    const userName = 'UserName'
-   return ( 
+   return (
       <div className={`${navbar.cotainer}`}>
          <div className={`${navbar.top}`}>
             <div className={`${navbar.logo}`}>
-               <img className={`${navbar.logoImage}`} src='img/logo.png' width='100px' height='40px'/>
+               <img className={`${navbar.logoImage}`} src='img/logo.png' width='100px' height='40px' />
             </div>
             <div className={`${navbar.user}`}>
-               <img className={`${navbar.userImage}`} src='img/avatar.png' width='50px' height='50px'/>
+               <img className={`${navbar.userImage}`} src='img/avatar.png' width='50px' height='50px' />
                <span className={`${navbar.userImage}`}>{userName}</span>
             </div>
          </div>
          <nav className={`${navbar.nav}`}>
             <ul className={`${navbar.navList}`}>
-               <li className={`${navbar.navLi} ${navbar.liProfile}`} >
-                  <Link className={`${navbar.navLink}`} to='/'>Profile</Link>
+               <li className={`${navbar.navLi}`} >
+                  <Link className={`${navbar.navLink}  ${navbar.navLinkHome}`} to='/'>Home</Link>
                </li>
-               <li className={`${navbar.navLi} ${navbar.liExplore}`} >
-                  <Link className={`${navbar.navLink}`} to='/'>Explore</Link>
+               <li className={`${navbar.navLi}`} >
+                  <Link className={`${navbar.navLink}  ${navbar.navLinkProfile}`} to='/'>Profile</Link>
                </li>
-               <li className={`${navbar.navLi} ${navbar.liNotifications}`} >
-                  <Link className={`${navbar.navLink}`} to='/'>Notifications</Link>
+               <li className={`${navbar.navLi}`} >
+                  <Link className={`${navbar.navLink}  ${navbar.navLinkExplore}`} to='/'>Explore</Link>
                </li>
-               <li className={`${navbar.navLi} ${navbar.liCreator}`} >
-                  <Link className={`${navbar.navLink}`} to='/'>About creator</Link>
+               <li className={`${navbar.navLi}`} >
+                  <Link className={`${navbar.navLink}  ${navbar.navLinkNotifications}`} to='/'>Notifications</Link>
                </li>
             </ul>
          </nav>
