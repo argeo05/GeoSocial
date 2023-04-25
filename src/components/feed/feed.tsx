4a2e7dae12f7bf5703dd6f5969fec31d525f3bce
Georgiy <argeo05@mail.ru>
2023-04-25 23:28:11 +0300

    feed is made up

--- src/components/feed/feed.tsx
+++ src/components/feed/feed.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
+import style from './feed.module.scss'
+import index from '../../index.module.scss'
+import { Link } from 'react-router-dom'
+import { PROFILE_ROUTE } from '../../utils/const'
 
-interface Props {}
+interface Props { }
 
 function Feed(props: Props) {
-   const {} = props
+   const { } = props
+   const photoSrc = '../../../public/img/avatar.png'
 
    return (
-      <p>Feed</p>
+      <section className={`${style.container}`}>
+         <h2 className={`${index.visuallyHidden}`}>Основная секция</h2>
+         <article className={`${style.dayBlock}`} >
+            <div className={`${style.dayPhoto}`}>
+               <h3 className={`${style.dayPhotoLabel}`}>Фото дня</h3>
+               <img src='img' className={`${style.dayPhotoImg}`} />
+            </div>
+         </article>
+         <div className={`${style.newsBlock}`}>
+            <ul className={`${style.newsFilters}`}>
+               <li className={`${style.newsFilter}`}>Newest</li>
+               <li className={`${style.newsFilter}`}>Most popular</li>
+            </ul>
+            <article className={`${style.post}`}>
+               <div className={`${style.postUser}`}>
+                  <Link to={PROFILE_ROUTE} className={`${style.userName}`} >UserName</Link>
+                  <img className={`${style.userImage}`} src='img/avatar.png' width='35px' height='35px' />
+               </div>
+               <div className={`${style.postText}`}>
+                  <p className={`${style.postParagraph}`}>
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's: bit.ly/2kvf6yj
+                  </p>
+                  {
+                     photoSrc ?
+                        <img className={`${style.postImage}`} src={photoSrc} /> :
+                        null
+                  }
+               </div>
+               <div className={`${style.postControl}`}>
+                  <button className={`${style.postLike}`}>Like</button>
+                  <button className={`${style.postReport}`}>Report</button>
+               </div>
+            </article>
+         </div>
+      </section>
    )
 }
 
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -2,6 +2,7 @@ import Feed from "../../components/feed/feed"
 import Navbar from "../../components/navbar/navbar"
 import SideBlock from "../../components/side-block/side-block"
 import index from '../../index.module.scss'
+import style from './main-page.module.scss'
 
 interface Props { }
 
@@ -10,13 +11,14 @@ function MainPage(props: Props) {
 
    return (
       <div className={`${index.page} ${index.container}`}>
-            <header className="header">
-               <Navbar />
-            </header>
-            <main>
-               <Feed />
-               <SideBlock />
-            </main>
+         <header className="header">
+            <Navbar />
+         </header>
+         <main className={`${style.main}`}>
+            <h1 className={`${index.visuallyHidden}`}>GeoSocial - tenth social site</h1>
+            <Feed />
+            <SideBlock />
+         </main>
       </div >
    )
 }
